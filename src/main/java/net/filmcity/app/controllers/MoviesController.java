fcba307466049620faddf006014299ad9e3d5e84
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-11-26 09:55:32 +0100

    Añadir películas al catálogo - user story hecho

--- src/main/java/net/filmcity/app/controllers/MoviesController.java
+++ src/main/java/net/filmcity/app/controllers/MoviesController.java
@@ -3,8 +3,12 @@ package net.filmcity.app.controllers;
 import net.filmcity.app.domain.Movie;
 import net.filmcity.app.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +26,10 @@ public class MoviesController {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+    @PostMapping("/movies")
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
 }
