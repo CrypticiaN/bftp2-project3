312738a33f2b69ba0d151c865814b7dc4bf90672
Alisa-InWonderland <alisa_ne@yahoo.com>
2021-12-01 11:38:30 +0100

    he cambiado coverImage para 5 peliculas en SampleDataLoader

--- src/main/java/net/filmcity/app/controllers/MoviesController.java
+++ src/main/java/net/filmcity/app/controllers/MoviesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@CrossOrigin
 public class MoviesController {
 
     private final MovieRepository movieRepository;
